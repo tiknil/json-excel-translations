c70e0c8907fe4be7563d65ec70de94e9c5eb3231
Giorgio Balduzzi <balduzzi.giorgio@tiknil.com>
2019-02-28 17:35:07 +0100

    Initial commit

---
+++ excelToJson.py
@@ -0,0 +1,88 @@
+import json
+import pandas as pd
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Generate JSON files from the excel.')
+
+parser.add_argument(
+    "-k",
+    "--key-name",
+    dest="key_name",
+    help="Name of the Excel column storing the keys (defualt: 'key') ",
+    default="key"
+)
+
+parser.add_argument(
+    "-i",
+    "--input-file",
+    dest="input_file",
+    help="Name of the input file (default 'output/translations.xlsx')",
+    default="output/translations.xlsx"
+)
+
+parser.add_argument(
+    "-o",
+    "--output-dir",
+    dest="output_dir",
+    help="Dir where the output files will be written (default 'output/')",
+    default="output/"
+)
+
+parser.add_argument(
+    "-e",
+    "--empty",
+    dest="empty_flag",
+    help="Set this flag if you want to include the empty values in your output Json (default is not including the key)",
+    action="store_true"
+)
+
+parser.add_argument(
+    "-is",
+    "--indent-size",
+    dest="indent_size",
+    help="Size of the output Json indentation (default 4)",
+    default=4
+)
+
+args = parser.parse_args()
+print(args)
+
+file = args.input_file
+key_field = args.key_name
+output_dir = args.output_dir.strip(' /')
+
+xl = pd.ExcelFile(file)
+df = xl.parse().fillna('')
+
+data = df.values.tolist()
+
+langs = list(df)
+langs.remove(key_field)  # Elimino header "key" che non è un linguaggio
+
+lang_translations = {}
+
+for lang in langs:
+    lang_translations[lang] = {}
+
+for index, row in df.iterrows():
+    key = row['key']
+
+    steps = key.split('.')
+
+    for lang in langs:
+        lang_trans = lang_translations[lang]
+        for idx, step in enumerate(steps):
+            if step not in lang_trans and idx != len(steps)-1:
+                lang_trans[step] = {}
+            elif idx != len(steps)-1:
+                lang_trans = lang_trans[step]
+        if args.empty_flag or row[lang] != '':
+            lang_trans[steps[-1]] = row[lang]
+
+for lang in langs:
+    out_file = open(f"{output_dir}/{lang}.json", 'w')
+    json.dump(lang_translations[lang], out_file, indent=args.indent_size, ensure_ascii=False)
+    out_file.close()
+
+
+
---
+++ jsonToExcel.py
@@ -0,0 +1,97 @@
+import json
+import os
+import pandas as pd
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Create an Excel file from a list of json translation files.')
+
+parser.add_argument(
+    "-p",
+    "--primary",
+    dest="primary",
+    help="Primary lang (default 'en')",
+    default="en"
+)
+
+parser.add_argument(
+    "-i",
+    "--input-dir",
+    dest="input_dir",
+    help="Dir of the json files",
+    default="translations/"
+)
+
+parser.add_argument(
+    "-o",
+    "--output-file",
+    dest="output_file",
+    help="Name of the output file",
+    default="output/translations.xlsx"
+)
+
+args = parser.parse_args()
+
+def loop_object(base_key, lang_data):
+    for key, value in lang_data.items():
+        key = f"{base_key}{'.' if base_key is not '' else ''}{key}"
+        if type(value) is dict:
+            loop_object(key, value)
+
+        elif type(value) is str:
+            # Controllo la presenza nelle altre lingue
+            key_translations = [value]
+            for lang_code in langs:
+                if lang_code == primary:
+                    continue
+
+                lang_translation = ""
+                lang_object = data_by_lang[lang_code]
+                steps = key.split('.')
+                for step in steps:
+                    if step in lang_object:
+                        lang_object = lang_object[step]
+                    else:
+                        break
+                if type(lang_object) is str:
+                    lang_translation = lang_object
+                key_translations.append(lang_translation)
+
+            output.append([key] + key_translations)
+
+primary = args.primary
+
+input_dir = args.input_dir.strip(' /')
+output_file = args.output_file.strip(' ')
+
+cwd = os.getcwd()
+os.chdir(input_dir)
+files = os.listdir('.')
+
+langs = []
+for file in files:
+    splitted = file.split('.')
+    if len(splitted) >= 2 and splitted[1] == 'json':
+        langs.append(splitted[0])
+
+os.chdir("../")
+
+output = []
+
+data_by_lang = {}
+
+for lang in langs:
+    inFile = open(f'{input_dir}/{lang}.json', 'r')
+    json_data = inFile.read()
+
+    data_by_lang[lang] = json.loads(json_data)
+    inFile.close()
+
+base_data = data_by_lang[primary]
+
+loop_object("", base_data)
+
+df = pd.DataFrame(output, columns=(['key'] + langs))
+
+writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+df.to_excel(writer, index=False)
+writer.save()
