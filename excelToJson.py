6de72c5a32da16fee02d59b7e985e1f807eb8fc8
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2023-06-27 21:26:36 +0600

    Add support of language .json files with flat composite keys.

--- excelToJson.py
+++ excelToJson.py
@@ -1,7 +1,8 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 import json
-import pandas as pd
 from argparse import ArgumentParser
+
+import pandas as pd
 
 parser = ArgumentParser(description='Generate JSON files from the excel.')
 
@@ -9,7 +10,7 @@ parser.add_argument(
     "-k",
     "--key-name",
     dest="key_name",
-    help="Name of the Excel column storing the keys (default: 'key') ",
+    help="Name of the Excel column storing the keys (default: '%(default)s') ",
     default="key"
 )
 
@@ -17,7 +18,7 @@ parser.add_argument(
     "-i",
     "--input-file",
     dest="input_file",
-    help="Name of the input file (default 'output/translations.xlsx')",
+    help="Name of the input file (default '%(default)s')",
     default="output/translations.xlsx"
 )
 
@@ -25,7 +26,7 @@ parser.add_argument(
     "-o",
     "--output-dir",
     dest="output_dir",
-    help="Dir where the output files will be written (default 'output/')",
+    help="Dir where the output files will be written (default '%(default)s')",
     default="output/"
 )
 
@@ -41,8 +42,17 @@ parser.add_argument(
     "-is",
     "--indent-size",
     dest="indent_size",
-    help="Size of the output Json indentation (default 4)",
+    help="Size of the output Json indentation (default %(default)s)",
+    type=int,
     default=4
+)
+
+parser.add_argument(
+    "-fk",
+    "--flat-keys",
+    dest="split_steps",
+    help="Set this flag if you want to keep composite keys in your output Json (default is to split composite keys by .)",
+    action="store_false",
 )
 
 args = parser.parse_args()
@@ -50,6 +60,8 @@ args = parser.parse_args()
 file = args.input_file
 key_field = args.key_name
 output_dir = args.output_dir
+empty_flag = args.empty_flag
+split_steps = args.split_steps
 
 xl = pd.ExcelFile(file)
 df = xl.parse().fillna('')
@@ -66,28 +78,25 @@ for lang in langs:
 
 
 for index, row in df.iterrows():
-    key = row['key']
+    key = row[key_field].strip()
 
-    steps = key.split('.')
+    steps = key.split('.') if split_steps else (key,)
 
     for lang in langs:
         lang_trans = lang_translations[lang]
 
         for idx, step in enumerate(steps):
-            if step not in lang_trans and idx != len(steps)-1:
+            if step not in lang_trans and idx != len(steps) - 1:
                 lang_trans[step] = {}
                 lang_trans = lang_trans[step]
-            elif idx != len(steps)-1:
+            elif idx != len(steps) - 1:
                 lang_trans = lang_trans[step]
 
-        if args.empty_flag or row[lang] != '':
+        if empty_flag or row[lang]:
             if not isinstance(lang_trans, str):
-                lang_trans[steps[-1]] = row[lang].rstrip()
+                lang_trans[steps[-1]] = row[lang].strip()
 
 for lang in langs:
     out_file = open(f"{output_dir}/{lang}.json", 'w')
     json.dump(lang_translations[lang], out_file, indent=args.indent_size, ensure_ascii=False)
     out_file.close()
-
-
-
